abcd5e3b7d212098d19af6bf64319d1fd23f8d44
Samar Singh Kanojiya <69416909+SSKSamarSinghKanojiya@users.noreply.github.com>
2025-03-31 16:41:29 +0530

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,18 +2,6 @@ const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
-// Register User
-// exports.register = async (req, res) => {
-//     const { username, password, role } = req.body;
-//     try {
-//         const user = new User({ username, password, role });
-//         await user.save();
-//         res.status(201).json({ message: 'User registered successfully' });
-//     } catch (err) {
-//         res.status(400).json({ error: err.message });
-//     }
-// };
-
 exports.register = async (req, res) => {
   const { username, password, role } = req.body;
   try {
